1b354dff611365d20149b6c7f030b3dfb93ea971
Shreyas Pahune <pahuneshreyas@gmail.com>
2022-06-21 02:10:17 +0530

    importing api routes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const connectToDB = require("./db/conn");
 
-      const express = require('express');
-      const app = express();
-      require('dotenv').config();
-      const cors = require('cors');
+// middlewares
+app.use(cors());
+app.use(express.json());
 
-      // middlewares
-      app.use(cors());
-      app.use(express.json());
-      
-      // defining port
-      const PORT = process.env.PORT || 3001;
-      
-      // setting up an empty GET Route
-      app.get('/', (req, res)=>{res.json({message: "You've come to the right place... it's a GET request!!"})});
-      
-      // Starting Server on PORT
-      app.listen(PORT, () => console.log('Server started on PORT Number: ' + PORT))
-      
+// defining port
+const PORT = process.env.PORT || 3001;
+
+// connection to the DB
+connectToDB();
+
+// api routes
+app.use('/api/auth', require('./routes/register'));
+app.use('/api/auth', require('./routes/login'));
+
+// Starting Server on PORT
+app.listen(PORT, () => console.log("Server started on PORT Number: " + PORT));
