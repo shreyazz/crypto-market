7a84971add411112bcd6175a61542138e85b6f9c
Shreyas Pahune <pahuneshreyas@gmail.com>
2022-06-22 22:40:20 +0530

    added login form and secured favCryptos route

--- client/src/pages/FavCryptos.jsx
+++ client/src/pages/FavCryptos.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React from "react";
+import jwt_decode from "jwt-decode";
+import Alert from "react-bootstrap/Alert";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const FavCryptos = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setError(true);
+      setTimeout(() => {
+        navigate("/login", { replace: true });
+      }, 1500);
+    }
+    if (token) {
+      if(!jwt_decode(token).isVerified){
+        navigate("/login", { replace: true });
+      }
+    }
+  }, []);
+  const decideScreen = () => {
+    if(error){
+      return (
+        <div>
+          <Alert variant="danger">You are not authorized</Alert>
+        </div>
+      )
+    }
+    else{
+      return (
+        <div>
+          <h1>Welcome</h1>
+        </div>
+      )
+    }
+  }
   return (
-    <div>FavCryptos</div>
-  )
-}
+    decideScreen()
+  );
+};
 
-export default FavCryptos
+export default FavCryptos;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
+  const [loginDetails, setLoginDetails] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) =>
+    setLoginDetails({ ...loginDetails, [e.target.name]: e.target.value });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("http://localhost:8080/api/auth/login", {
+      email: loginDetails.email,
+      password: loginDetails.password,
+    });
+    localStorage.setItem("token", res.data.token);
+    setError(false);
+    navigate("/", { replace: true });
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <Container md={10} className="mt-5 login-container">
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => handleChange(e)}
+            name="email"
+          />
+          <Form.Text className="text-muted">
+            Don't worry we wont share it with anyone.
+          </Form.Text>
+        </Form.Group>
 
-export default Login
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => handleChange(e)}
+            name="password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      {loginDetails.email} {loginDetails.password}
+    </Container>
+  );
+};
+
+export default Login;
